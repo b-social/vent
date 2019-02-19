f168e2e24aeae63b2cf99eeaacae32ecfbb2423f
Toby Clemson <tobyclemson@gmail.com>
2019-02-19 14:19:05 +0000

    Allow contextualisers to be added as part of a ruleset.
    A contextualiser fetches additional context when the corresponding
    event is matched and passes that context to each of the actions.

--- src/vent/core.clj
+++ src/vent/core.clj
@@ -5,6 +5,9 @@
 (defprotocol Action
   (execute [this context]))
 
+(defprotocol Contextualiser
+  (add-context-to [this context]))
+
 (def ^:private default-event-type-fn
   (fn [event] (keyword (get-in event [:payload :type]))))
 (def ^:private default-event-channel-fn
@@ -19,14 +22,17 @@
 (defn- collect-from
   ([fragments key] (collect-from fragments key :defaults {}))
   ([fragments key & {:keys [defaults]}]
-    (let [relevant (concat [{key defaults}] (having-key fragments key))
-           extracted {key (merge-over (mapv key relevant))}]
-      extracted)))
+   (let [relevant (concat [{key defaults}] (having-key fragments key))
+         extracted {key (merge-over (mapv key relevant))}]
+     extracted)))
 
 (defn- return-on-match [rule event {:keys [event-type-fn]}]
   (and
     (= (:event-type rule) (event-type-fn event))
     rule))
+
+(defn- resolve-handlers [rule handlers-key event context]
+  (map #((:handler %) event context) (handlers-key rule)))
 
 (defn ruleset [& fragments]
   (let [options (collect-from fragments :options
@@ -42,36 +48,65 @@
   {:rules {(keyword channel) event-rules}})
 
 (defn on [event-type & handlers]
-  {:event-type event-type
-   :handlers   handlers})
+  {:event-type              event-type
+   :contextualiser-handlers (filterv #(= (:type %) :contextualiser) handlers)
+   :action-handlers         (filterv #(= (:type %) :action) handlers)})
 
-(defn determine-actions [ruleset event context]
+(defn act [act-handler]
+  {:type    :action
+   :handler act-handler})
+
+(defn contextualise [contextualise-handler]
+  {:type    :contextualiser
+   :handler contextualise-handler})
+
+(defn plan [& {:keys [contextualisers actions]
+               :or   {contextualisers []
+                      actions         []}}]
+  {:contextualisers contextualisers
+   :actions         actions})
+
+(defn- rule->plan [event context]
+  (fn [rule]
+    (let [contextualisers
+          (resolve-handlers
+            rule :contextualiser-handlers
+            event context)
+
+          actions
+          (resolve-handlers
+            rule :action-handlers
+            event context)]
+      (plan
+        :contextualisers contextualisers
+        :actions actions))))
+
+(defn- rule-for-event? [event options]
+  (fn [rule] (return-on-match rule event options)))
+
+(defn determine-plans [ruleset event context]
   (let [{:keys [options rules]} ruleset
         {:keys [event-channel-fn]} options
 
         channel-rules (or ((event-channel-fn event) rules) [])
 
         event-rules
-        (filterv
-          (fn [rule] (return-on-match rule event options))
-          channel-rules)
+        (filterv (rule-for-event? event options) channel-rules)
 
-        event-actions
-        (mapcat
-          (fn [rule]
-            (map #(% event context) (:handlers rule)))
-          event-rules)]
-    event-actions))
+        event-plans
+        (mapv (rule->plan event context) event-rules)]
+    event-plans))
 
-
-; Needs testing
 (defn execute-action [action context]
   (execute action context))
 
-(defn execute-actions [actions context]
-  (doseq [action actions]
-    (execute-action action context)))
+(defn execute-plan [plan context]
+  (mapv #(execute-action % context) (:actions plan)))
+
+(defn execute-plans [plans context]
+  (mapv #(
+           execute-plan % context) plans))
 
 (defn react-to [ruleset event context]
-  (let [actions (determine-actions ruleset event context)]
-    (execute-actions actions context)))
+  (let [plans (determine-plans ruleset event context)]
+    (execute-plans plans context)))
--- test/vent/core_test.clj
+++ test/vent/core_test.clj
@@ -2,39 +2,56 @@
   (:require
     [clojure.test :refer :all]
 
-    [clj-fakes.core
-     :refer [with-fakes
-             recorded-fake
-             patch!
-             was-called]]
+    [clj-fakes.core :as fakes]
 
     [halboy.resource :as hal]
 
     [vent.test-support.data :as data]
 
-    [vent.core :as vent]
-    [vent.hal :as vent-hal]))
+    [vent.core :as v]
+    [vent.hal :as vhal]))
 
-(defrecord TestAction [identifier event context]
-  vent/Action
+(defrecord CapturingAction [identifier event context]
+  v/Action
   (execute [_ _]))
 
-(defn test-action [& {:as options}]
-  (map->TestAction options))
+(defn capturing-action [& {:as options}]
+  (map->CapturingAction options))
 
 (defn capture-as [identifier]
   (fn [event context]
-    (test-action
+    (capturing-action
       :identifier identifier
       :event event
       :context context)))
 
+(defrecord FakeAction [fake]
+  v/Action
+  (execute [_ context]
+    (fake context)))
+
+(defn fake-action [& {:as options}]
+  (map->FakeAction options))
+
+(defrecord MergingContextualiser [context-to-merge]
+  v/Contextualiser
+  (add-context-to [_ context]
+    (merge context context-to-merge)))
+
+(defn merging-contextualiser [& {:as options}]
+  (map->MergingContextualiser options))
+
+(defn add-from-map [extra-context]
+  (fn [_ _]
+    (merging-contextualiser
+      :context-to-merge extra-context)))
+
 (deftest generates-action-when-event-matched
   (let [ruleset
-        (vent/ruleset
-          (vent/from :some-event-channel
-            (vent/on :some-event-type
-              (capture-as :some-action))))
+        (v/ruleset
+          (v/from :some-event-channel
+            (v/on :some-event-type
+              (v/act (capture-as :some-action)))))
 
         event-channel "some-event-channel"
         event-payload
@@ -47,20 +64,21 @@
 
         context {:thing "thing"}
 
-        actions (vent/determine-actions ruleset event context)]
-    (is (= [(test-action
-              :identifier :some-action
-              :event event
-              :context context)]
-          actions))))
+        plans (v/determine-plans ruleset event context)]
+    (is (= [(v/plan
+              :actions [(capturing-action
+                          :identifier :some-action
+                          :event event
+                          :context context)])]
+          plans))))
 
 (deftest allows-multiple-actions-for-the-same-event
   (let [ruleset
-        (vent/ruleset
-          (vent/from :some-event-channel
-            (vent/on :some-event-type
-              (capture-as :action-1)
-              (capture-as :action-2))))
+        (v/ruleset
+          (v/from :some-event-channel
+            (v/on :some-event-type
+              (v/act (capture-as :action-1))
+              (v/act (capture-as :action-2)))))
 
         event-channel "some-event-channel"
         event-payload
@@ -68,30 +86,31 @@
          :message "An important message"}
 
         event
-        {:channel  event-channel
+        {:channel event-channel
          :payload event-payload}
 
         context {}
 
-        actions (vent/determine-actions ruleset event context)]
-    (is (= [(test-action
-              :identifier :action-1
-              :event event
-              :context context)
-            (test-action
-              :identifier :action-2
-              :event event
-              :context context)]
-          actions))))
+        plans (v/determine-plans ruleset event context)]
+    (is (= [(v/plan
+              :actions [(capturing-action
+                          :identifier :action-1
+                          :event event
+                          :context context)
+                        (capturing-action
+                          :identifier :action-2
+                          :event event
+                          :context context)])]
+          plans))))
 
 (deftest correctly-determines-actions-when-many-event-types-are-defined
   (let [ruleset
-        (vent/ruleset
-          (vent/from :some-event-channel
-            (vent/on :some-event-type
-              (capture-as :some-action))
-            (vent/on :other-event-type
-              (capture-as :other-action))))
+        (v/ruleset
+          (v/from :some-event-channel
+            (v/on :some-event-type
+              (v/act (capture-as :some-action)))
+            (v/on :other-event-type
+              (v/act (capture-as :other-action)))))
 
         event-channel "some-event-channel"
         event-payload
@@ -104,22 +123,23 @@
 
         context {}
 
-        actions (vent/determine-actions ruleset event context)]
-    (is (= [(test-action
-              :identifier :other-action
-              :event event
-              :context context)]
-          actions))))
+        plans (v/determine-plans ruleset event context)]
+    (is (= [(v/plan
+              :actions [(capturing-action
+                          :identifier :other-action
+                          :event event
+                          :context context)])]
+          plans))))
 
 (deftest correctly-determines-actions-when-many-event-channels-are-defined
   (let [ruleset
-        (vent/ruleset
-          (vent/from :first-event-channel
-            (vent/on :some-event-type
-              (capture-as :first-channel-action)))
-          (vent/from :second-event-channel
-            (vent/on :some-event-type
-              (capture-as :second-channel-action))))
+        (v/ruleset
+          (v/from :first-event-channel
+            (v/on :some-event-type
+              (v/act (capture-as :first-channel-action))))
+          (v/from :second-event-channel
+            (v/on :some-event-type
+              (v/act (capture-as :second-channel-action)))))
 
         event-channel "second-event-channel"
         event-payload
@@ -131,25 +151,26 @@
 
         context {}
 
-        actions (vent/determine-actions ruleset event context)]
-    (is (= [(test-action
-              :identifier :second-channel-action
-              :event event
-              :context context)]
-          actions))))
+        plans (v/determine-plans ruleset event context)]
+    (is (= [(v/plan
+              :actions [(capturing-action
+                          :identifier :second-channel-action
+                          :event event
+                          :context context)])]
+          plans))))
 
 (deftest allows-event-type-lookup-function-to-be-overridden
   (let [ruleset
-        (vent/ruleset
-          (vent/options
-            :event-type-fn (vent-hal/event-type-property :type))
+        (v/ruleset
+          (v/options
+            :event-type-fn (vhal/event-type-property :type))
 
-          (vent/from :first-event-channel
-            (vent/on :some-event-type
-              (capture-as :first-channel-action)))
-          (vent/from :second-event-channel
-            (vent/on :some-event-type
-              (capture-as :second-channel-action))))
+          (v/from :first-event-channel
+            (v/on :some-event-type
+              (v/act (capture-as :first-channel-action))))
+          (v/from :second-event-channel
+            (v/on :some-event-type
+              (v/act (capture-as :second-channel-action)))))
 
         event-channel "second-event-channel"
         event-resource
@@ -164,25 +185,26 @@
 
         context {}
 
-        actions (vent/determine-actions ruleset event context)]
-    (is (= [(test-action
-              :identifier :second-channel-action
-              :event event
-              :context context)]
-          actions))))
+        plans (v/determine-plans ruleset event context)]
+    (is (= [(v/plan
+              :actions [(capturing-action
+                          :identifier :second-channel-action
+                          :event event
+                          :context context)])]
+          plans))))
 
 (deftest allows-event-channel-lookup-function-to-be-overridden
   (let [ruleset
-        (vent/ruleset
-          (vent/options
+        (v/ruleset
+          (v/options
             :event-channel-fn (fn [event] (keyword (:topic event))))
 
-          (vent/from :first-event-channel
-            (vent/on :some-event-type
-              (capture-as :first-channel-action)))
-          (vent/from :second-event-channel
-            (vent/on :some-event-type
-              (capture-as :second-channel-action))))
+          (v/from :first-event-channel
+            (v/on :some-event-type
+              (v/act (capture-as :first-channel-action))))
+          (v/from :second-event-channel
+            (v/on :some-event-type
+              (v/act (capture-as :second-channel-action)))))
 
         event-channel "second-event-channel"
         event-payload
@@ -190,14 +212,76 @@
          :message "The message"}
 
         event
-        {:topic event-channel
+        {:topic   event-channel
          :payload event-payload}
 
         context {}
 
-        actions (vent/determine-actions ruleset event context)]
-    (is (= [(test-action
-              :identifier :second-channel-action
-              :event event
-              :context context)]
-          actions))))
+        plans (v/determine-plans ruleset event context)]
+    (is (= [(v/plan
+              :actions [(capturing-action
+                          :identifier :second-channel-action
+                          :event event
+                          :context context)])]
+          plans))))
+
+(deftest allows-event-specific-context-to-be-generated
+  (let [context {:first  1
+                 :second 2}
+        extra-context {:second 3
+                       :third 4}
+
+        ruleset
+        (v/ruleset
+          (v/from :event-channel
+            (v/on :event-type
+              (v/contextualise (add-from-map extra-context))
+              (v/act (capture-as :action)))))
+
+        event-channel "event-channel"
+        event-payload
+        {:type    "event-type"
+         :message "The message"}
+
+        event
+        {:channel event-channel
+         :payload event-payload}
+
+        plans (v/determine-plans ruleset event context)]
+    (is (= [(v/plan
+              :contextualisers [(merging-contextualiser
+                                  :context-to-merge extra-context)]
+              :actions [(capturing-action
+                          :identifier :action
+                          :event event
+                          :context context)])]
+          plans))))
+
+(deftest executes-the-provided-action-in-the-provided-context
+  (fakes/with-fakes
+    (let [fake (fakes/recorded-fake [[fakes/any] "some-result"])
+          action (fake-action :fake fake)
+          plan (v/plan :actions [action])
+          context {:important :value}
+
+          result (v/execute-plan plan context)]
+      (is (fakes/was-called-once fake [context]))
+      (is (= result ["some-result"])))))
+
+(deftest executes-all-provided-actions-in-the-provided-context
+  (fakes/with-fakes
+    (let [fake1 (fakes/recorded-fake [[fakes/any] "first-result"])
+          fake2 (fakes/recorded-fake [[fakes/any] "second-result"])
+
+          action1 (fake-action :fake fake1)
+          action2 (fake-action :fake fake2)
+
+          plan1 (v/plan :actions [action1])
+          plan2 (v/plan :actions [action2])
+
+          context {:important :value}
+
+          results (v/execute-plans [plan1 plan2] context)]
+      (is (fakes/was-called-once fake1 [context]))
+      (is (fakes/was-called-once fake2 [context]))
+      (is (= results [["first-result"] ["second-result"]])))))
