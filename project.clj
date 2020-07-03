ba66b46d2269832752ba34678f2daa29771f330e
Alex Parlett <alexparlett@gmail.com>
2020-07-03 11:20:02 +0100

    Version 0.6.7-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject b-social/vent "0.6.6"
+(defproject b-social/vent "0.6.7-SNAPSHOT"
   :description "Rule based event processing engine."
   :url "https://github.com/b-social/vent"
   :license {:name "The MIT License"
